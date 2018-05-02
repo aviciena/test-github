69fbd35d0c9fa8d7522114a88422782d55e61448
Ibnu Siena <aviciena.siena@gmail.com>
2018-05-02 13:31:59 +0700

    Initial Commit.

---
+++ TestProject/TestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Ibnu Siena on 5/2/18.
+//  Copyright © 2018 Ibnu Siena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
